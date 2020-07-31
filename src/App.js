32305d006d87b98f93ab357e43b0e38d9d2b4cd2
khai <khai@user.sg>
2020-07-31 16:18:31 +0700

    Get product

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // in src/App.js
-import * as React from "react"
-import { Admin, Resource } from "react-admin"
-import jsonServerProvider from "ra-data-json-server"
-import { UserList } from "./users"
+import * as React from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./users";
+import { ProductList } from "./products";
 
-const dataProvider = jsonServerProvider("http://localhost:4000/api/")
+const dataProvider = jsonServerProvider("http://192.168.1.2:4000/api/");
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="buyers" list={UserList}></Resource>
+    <Resource name="products" list={ProductList}></Resource>
   </Admin>
-)
+);
 
-export default App
+export default App;
---
+++ src/products.js
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { List, EditButton } from "react-admin";
+import inflection from "inflection";
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: "1em",
+  },
+  media: {
+    height: 140,
+  },
+  title: {
+    paddingBottom: "0.5em",
+  },
+  actionSpacer: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+});
+
+const CategoryGrid = (props) => {
+  const classes = useStyles(props);
+  const { data, ids } = props;
+  return ids ? (
+    <Grid container spacing={2} className={classes.root}>
+      {ids.map((id) => (
+        <Grid key={id} xs={12} sm={6} md={4} lg={3} xl={2} item>
+          <Card>
+            <CardMedia
+              image={`https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdmiPPTUIqwFi0LNvibcr7IMyFHxsM2GnA5Q&usqp=CAU`}
+              className={classes.media}
+            />
+            <CardContent className={classes.title}>
+              <Typography variant="h5" component="h2" align="center">
+                {inflection.humanize(data[id].productName)}
+              </Typography>
+            </CardContent>
+            <CardContent className={classes.title}>
+              <Typography variant="h5" component="h2" align="center">
+                {inflection.humanize(data[id].currentPrice)}
+              </Typography>
+            </CardContent>
+            <CardActions classes={{ spacing: classes.actionSpacer }}>
+              <EditButton />
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  ) : null;
+};
+
+export const ProductList = (props) => (
+  <List
+    {...props}
+    sort={{ field: "name", order: "ASC" }}
+    perPage={20}
+    pagination={false}
+    component="div"
+    actions={false}
+  >
+    {/* 
+    // @ts-ignore */}
+    <CategoryGrid />
+  </List>
+);
