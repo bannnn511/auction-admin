a6d69771677d8206e294ad04297b8e6ee684d1a8
An <habinhan1999@gmail.com>
2020-08-03 11:24:03 +0700

    hotfix logout

--- src/authProvider.js
+++ src/authProvider.js
@@ -3,6 +3,7 @@ import decodeJwt from 'jwt-decode';
 
 // called when the user attempts to log in
 async function login({ email, password }) {
+  console.log('Login');
   localStorage.setItem('email', email);
   localStorage.setItem('password', password);
   const url = URL + '/auth/login';
@@ -30,40 +31,49 @@ async function login({ email, password }) {
 
 // called when the user clicks on the logout button
 async function logout() {
+  console.log('Logout');
   const token = localStorage.getItem('token');
-  console.log('=======', decodeJwt(token));
-  if (decodeJwt(token).exp < new Date().getTime() / 1000) {
-    const username = localStorage.getItem('email');
-    const password = localStorage.getItem('password');
-    await login(username, password);
-    console.log('EXPIRED');
-  }
-  const url = URL + '/auth/logout';
-  console.log(url);
-  const request = await new Request(url, {
-    method: 'GET',
-    //body: JSON.stringify({ email: username, password }),
-    headers: new Headers({
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    }),
-  });
-
-  return await fetch(request)
-    .then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then(() => {
-      localStorage.removeItem('token');
-      localStorage.removeItem('permissions');
+  if (token) {
+    if (!token) console.log('=======', decodeJwt(token));
+    if (decodeJwt(token).exp < new Date().getTime() / 1000) {
+      const email = localStorage.getItem('email');
+      const password = localStorage.getItem('password');
+      console.log(email, password);
+      await login(email, password);
+      console.log('EXPIRED');
+    }
+    const url = URL + '/auth/logout';
+    console.log(url);
+    const request = await new Request(url, {
+      method: 'GET',
+      //body: JSON.stringify({ email: username, password }),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      }),
     });
+
+    return await fetch(request)
+      .then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(() => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('permissions');
+      });
+  } else {
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+    return Promise.resolve();
+  }
 }
 
 // called when the API returns an error
 function checkError({ status }) {
+  console.log('check error');
   if (status === 401 || status === 403) {
     localStorage.removeItem('token');
     return Promise.reject();
@@ -73,6 +83,7 @@ function checkError({ status }) {
 
 // called when the user navigates to a new location, to check for authentication
 function checkAuth() {
+  console.log('check auth');
   return localStorage.getItem('token')
     ? Promise.resolve()
     : Promise.reject({ redirectTo: '/no-access' });
@@ -80,6 +91,7 @@ function checkAuth() {
 
 // called when the user navigates to a new location, to check for permissions / roles
 function getPermissions() {
+  console.log('get permissions');
   const role = localStorage.getItem('permissions');
   return role ? Promise.resolve(role) : Promise.reject();
 }
